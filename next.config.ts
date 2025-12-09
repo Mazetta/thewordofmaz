421533c129aeac383d8785dc727cf570b053410c
Mazeriio <110097914+Mazetta@users.noreply.github.com>
2025-12-09 23:45:23 +0000

    feat: configure remote image patterns in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "www.mazeriio.net",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
