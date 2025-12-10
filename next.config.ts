aa08f9458d671b0b167dfd14a9233de5b7e23c35
Mazeriio <110097914+Mazetta@users.noreply.github.com>
2025-12-10 18:12:41 +0000

    test

--- next.config.ts
+++ next.config.ts
@@ -7,7 +7,16 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "www.mazeriio.net",
       },
+      {
+        protocol: "https",
+        hostname: "prod-files-secure.s3.us-west-2.amazonaws.com",
+      },
+      {
+        protocol: "https",
+        hostname: "s3.us-west-2.amazonaws.com",
+      },
     ],
+    unoptimized: true, // ⚠️ important pour les images Notion avec jetons temporaires
   },
 };
 
