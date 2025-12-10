8dbfe4d3b2547200132d2c6327da919e402e7554
Mazeriio <110097914+Mazetta@users.noreply.github.com>
2025-12-10 15:35:24 +0000

    experiment

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -125,7 +125,7 @@ export default async function PostPage({ params }: PostPageProps) {
       />
       <article className="max-w-3xl mx-auto prose dark:prose-invert">
         {post.coverImage && (
-          <div className="relative aspect-video w-full mb-8 rounded-b-lg rounded-t-lg overflow-hidden">
+          <div className="relative aspect-video w-full mb-8 overflow-hidden">
             <Image
               src={post.coverImage}
               alt={post.title}
