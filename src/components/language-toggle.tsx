add247de4d7680746660ca73dec8950956d761b9
Mazeriio <110097914+Mazetta@users.noreply.github.com>
2025-12-15 19:02:39 +0000

    test

--- src/components/language-toggle.tsx
+++ src/components/language-toggle.tsx
@@ -44,7 +44,7 @@ export function LanguageToggle() {
       <DropdownMenuTrigger asChild>
         <div
           className={cn(
-            "flex h-8 w-8 items-center justify-center rounded-md cursor-pointer transition-all border",
+            "flex h-8 w-8 items-center justify-center rounded-md cursor-pointer transition-all border outline-none",
             isDark
               ? "bg-zinc-950 border-zinc-800 hover:bg-zinc-900"
               : "bg-white border-zinc-200 hover:bg-zinc-100"
